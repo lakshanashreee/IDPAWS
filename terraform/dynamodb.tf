9161d0076c8edf7e3f4b91821b4126504ec0b998
lakshanashreee <lakshanalakshu2408@gmail.com>
2026-08-05 15:10:11 +0530

    Fixed some more vulnerabilities.

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,3 +1,14 @@
+resource "aws_kms_key" "dynamodb_key" {
+  description             = "KMS key for DynamoDB tables"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "dynamodb_key_alias" {
+  name          = "alias/ecommerce-dynamodb-key"
+  target_key_id = aws_kms_key.dynamodb_key.key_id
+}
+
 resource "aws_dynamodb_table" "product_table" {
   name         = "L_ProductTable"
   billing_mode = "PAY_PER_REQUEST"
@@ -6,6 +17,15 @@ resource "aws_dynamodb_table" "product_table" {
   attribute {
     name = "productId"
     type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamodb_key.arn
   }
 
   tags = merge(
@@ -26,6 +46,15 @@ resource "aws_dynamodb_table" "inventory_table" {
     type = "S"
   }
 
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamodb_key.arn
+  }
+
   tags = merge(
     local.common_tags,
     {
@@ -42,6 +71,15 @@ resource "aws_dynamodb_table" "cart_table" {
   attribute {
     name = "userId"
     type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamodb_key.arn
   }
 
   tags = merge(
@@ -73,6 +111,15 @@ resource "aws_dynamodb_table" "order_table" {
     projection_type = "ALL"
   }
 
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamodb_key.arn
+  }
+
   tags = merge(
     local.common_tags,
     {
@@ -100,6 +147,15 @@ resource "aws_dynamodb_table" "payment_table" {
     name            = "orderId-index"
     hash_key        = "orderId"
     projection_type = "ALL"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamodb_key.arn
   }
 
   tags = merge(
